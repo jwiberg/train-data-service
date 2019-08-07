7486cb57425758fb4af5d5d6b0d3e3e5f3ed4e98
Juha Wiberg <juha.wiberg@ambientia.fi>
2019-08-07 14:57:32 +0300

    Api for queyring train data added.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const port = process.env.PORT || 3000
 const app = express()
+const fs = require('fs')
+const low = require('lowdb')
+const Memory = require('lowdb/adapters/Memory')
+const db = low(new Memory)
 
-app.get('/', function (req, res) {
-  res.send(JSON.stringify({ Hello: 'World' }))
+app.get('/data_for_train/:train_id', function (req, res) {
+  res.send(JSON.stringify(db.get('trains.' + req.params.train_id).value()))
 })
 
 app.listen(port, function () {
+  //JSON.parse()
+  fs.readFile("trains.json", function (err, data) {
+    if (err) throw err;
+    db.defaults({trains: JSON.parse(data)})
+      .write()
+    console.log("Trains loaded to database.")
+  });
   console.log('Example app listening on port ' + port + '!')
 })
