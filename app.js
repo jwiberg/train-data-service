c11693c2b9125a8e2fd60d4e6b6957cc51e8dceb
Juha Wiberg <juha.wiberg@ambientia.fi>
2019-08-07 15:11:55 +0300

    Api skeleton for seat reservation added.

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const low = require('lowdb')
 const Memory = require('lowdb/adapters/Memory')
 const db = low(new Memory())
 
-app.get('/data_for_train/:train_id', function (req, res) {
+app.get('/data_for_train/:train_id', (req, res) => {
   res.send(JSON.stringify(db.get('trains.' + req.params.train_id).value()))
 })
 
-app.listen(port, function () {
-  fs.readFile('trains.json', function (err, data) {
+app.post('/reserve', (req, res) => {
+  res.send("Reserved or not")
+})
+
+app.listen(port, () => {
+  fs.readFile('trains.json', (err, data) => {
     if (err) throw err
     db.defaults({ trains: JSON.parse(data) })
       .write()
